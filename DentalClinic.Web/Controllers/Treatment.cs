990335fa04665b4228484eeeee7f7a0f8cdd3c0c
Martin Milev <marto379@abv.bg>
2023-07-08 19:27:03 +0300

    update

---
+++ DentalClinic.Web/Controllers/Treatment.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DentalClinicSystem.Web.Controllers
+{
+    [AllowAnonymous]
+    public class Treatment : Controller
+    {
+        public IActionResult Root()
+        {
+            return View();
+        }
+    }
+}
