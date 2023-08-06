f27d121f6eac97c1d20069263c9552898b180d9d
Martin Milev <marto379@abv.bg>
2023-08-06 18:47:21 +0300

    add unit tests project

---
+++ DentalClinicAppUnitTests/DentistServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using DentalClinicSystem.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentalClinicAppUnitTests
+{
+    internal class DentistServiceTest
+    {
+        private DentistService dentistService;
+
+        public DentistServiceTest(DentistService dentistService)
+        {
+            this.dentistService = dentistService;
+        }
+    }
+}
